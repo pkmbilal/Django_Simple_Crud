88774e17fbbbdd7d7f34d1b586d70b52c0335db2
Mohammed Bilal <bilal00717@gmail.com>
2023-09-08 20:03:09 +0300

    navigation bar updated in base.html

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='home'),
-    path('add/',views.addemp),
-    path('delete/',views.delemp),
-    path('update/',views.upemp),
+    path('',views.index,name='empList'),
+    path('add/',views.addemp,name='addEmployee'),
+    path('delete/',views.delemp,name='deleteEmployee'),
+    path('update/',views.upemp,name='updateEmployee'),
 ]
